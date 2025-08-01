76906e88c5d5a2a7daca664758790c2d5c4062ec
kirti-21536 <kirtichau1507@gmail.com>
2025-08-01 20:15:16 +0530

    Create 242-ValidAnagram

---
+++ 242-ValidAnagram/242-ValidAnagram.java
@@ -0,0 +1,11 @@
+// Last updated: 8/1/2025, 8:15:15 PM
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+        char[] a = s.toCharArray();
+        char[] b = t.toCharArray();
+        Arrays.sort(a);
+        Arrays.sort(b);
+        return Arrays.equals(a, b);
+    }
+}
