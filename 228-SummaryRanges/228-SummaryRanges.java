2f0abef25bc11d82c8f397d299493383a8d3d86b
kirti-21536 <kirtichau1507@gmail.com>
2025-08-01 20:15:24 +0530

    Create 228-SummaryRanges

---
+++ 228-SummaryRanges/228-SummaryRanges.java
@@ -0,0 +1,20 @@
+// Last updated: 8/1/2025, 8:15:23 PM
+class Solution {
+    public List<String> summaryRanges(int[] nums) {
+        List<String> list =new ArrayList();
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            int start=nums[i];
+            while(i+1<n &&nums[i+1]==nums[i]+1)i++;
+            if(start!=nums[i]){
+                list.add(""+start+"->"+nums[i]);
+
+            }
+            else{
+                list.add(""+start);
+            }
+            
+        }
+        return list;
+    }
+}
