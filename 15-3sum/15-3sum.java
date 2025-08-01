be939075b3315a59589b2c83498de985d69a3c9e
kirti-21536 <kirtichau1507@gmail.com>
2025-08-01 20:16:46 +0530

    Create 15-3sum

---
+++ 15-3sum/15-3sum.java
@@ -0,0 +1,41 @@
+// Last updated: 8/1/2025, 8:16:45 PM
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums); // Sort the array
+        List<List<Integer>> result = new ArrayList<>();
+
+        int n = nums.length;
+
+        for (int i = 0; i < n - 2; i++) {
+            // Skip duplicate elements for i
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            int left = i + 1;
+            int right = n - 1;
+
+            while (left < right) {
+                int total = nums[i] + nums[left] + nums[right];
+
+                if (total < 0) {
+                    left++;
+                } else if (total > 0) {
+                    right--;
+                } else {
+                    // Found a triplet
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+
+                    // Skip duplicates for left
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    // Skip duplicates for right
+                    while (left < right && nums[right] == nums[right - 1]) right--;
+
+                    // Move both pointers
+                    left++;
+                    right--;
+                }
+            }
+        }
+
+        return result;
+    }
+}
