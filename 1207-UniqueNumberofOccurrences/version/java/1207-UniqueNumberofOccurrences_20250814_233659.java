750ce22d660df042a5c0b5517764bc3942454350
kirti-21536 <kirtichau1507@gmail.com>
2025-08-14 23:36:59 +0530

    Create 1207-UniqueNumberofOccurrences

---
+++ 1207-UniqueNumberofOccurrences/version/java/1207-UniqueNumberofOccurrences_20250814_233659.java
@@ -0,0 +1,15 @@
+// Last updated: 8/14/2025, 11:36:59 PM
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i:arr){
+           map.put(i, map.getOrDefault(i, 0) + 1);
+        }
+        HashSet<Integer> set = new HashSet<>();
+        for(int freq : map.values()){
+            if(!set.add(freq)) return false;  // duplicate frequency found
+        }
+        return true;
+
+    }
+}
