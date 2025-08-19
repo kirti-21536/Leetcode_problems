0dc843f79a3efa3943a35e21513aadf5bb2e266a
kirti-21536 <kirtichau1507@gmail.com>
2025-08-19 22:37:17 +0530

    Create 2348-NumberofZero-FilledSubarrays

---
+++ 2348-NumberofZero-FilledSubarrays/version/java/2348-NumberofZero-FilledSubarrays_20250819_223712.java
@@ -0,0 +1,19 @@
+// Last updated: 8/19/2025, 10:37:12 PM
+class Solution {
+    public long zeroFilledSubarray(int[] nums) {
+        long total=0,count=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==0){
+                count++;
+            }
+            if(nums[i]!=0){
+                total=total+count*(count+1)/2;
+                count=0;
+            }
+        }
+        total+=count*(count+1)/2;
+        return total;
+
+        
+    }
+}
