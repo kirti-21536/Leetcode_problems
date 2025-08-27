996a815715bd44d9dc1392fcfe751dbdac92fa32
Kirti Chaudhary <kirtichau1507@gmail.com>
2025-08-28 01:09:42 +0530

    Create 561-ArrayPartition

---
+++ 561-ArrayPartition/version/java/561-ArrayPartition_20250828_010942.java
@@ -0,0 +1,14 @@
+// Last updated: 8/28/2025, 1:09:42 AM
+class Solution {
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int sum=0;
+        for(int i=0;i<nums.length;i+=2){
+            //we access the left element of a pair that is min in those 
+            // 2 elements in a pair after sorting
+            sum+=nums[i];
+        }
+        return sum;
+        
+    }
+}
