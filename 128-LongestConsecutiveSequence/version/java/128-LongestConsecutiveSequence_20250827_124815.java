f6814f887ff55e1a8652bb5252861d0c842a7b6c
Kirti Chaudhary <kirtichau1507@gmail.com>
2025-08-27 12:48:17 +0530

    Create 128-LongestConsecutiveSequence

---
+++ 128-LongestConsecutiveSequence/version/java/128-LongestConsecutiveSequence_20250827_124815.java
@@ -0,0 +1,30 @@
+// Last updated: 8/27/2025, 12:48:15 PM
+class Solution {
+    
+    public int longestConsecutive(int[] nums) {
+        // tc= O(n)
+        Set<Integer> set=new HashSet<>();
+        for(int i=0;i<nums.length;i++){
+            set.add(nums[i]);
+        }
+        int ans=0;
+        
+        for(int i=0;i<nums.length;i++){
+            if(!set.contains(nums[i]-1)){
+                int c=0;
+                int key=nums[i];
+                while(set.contains(key)){
+                    set.remove(key);
+                    c++;
+                    key++;
+
+                }
+                ans=Math.max(c,ans);
+            }
+        }
+        return ans;
+        
+
+        
+    }
+}
