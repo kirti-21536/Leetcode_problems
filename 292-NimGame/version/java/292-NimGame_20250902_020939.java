6ba78e0aeb80ffa3e099b5d799faabe98a594265
Kirti Chaudhary <kirtichau1507@gmail.com>
2025-09-02 02:09:40 +0530

    Create 292-NimGame

---
+++ 292-NimGame/version/java/292-NimGame_20250902_020939.java
@@ -0,0 +1,9 @@
+// Last updated: 9/2/2025, 2:09:39 AM
+class Solution {
+    public boolean canWinNim(int n) {
+        if(n<=2)return true;
+        if(n%4==0)return false;
+        return true;
+        
+    }
+}
