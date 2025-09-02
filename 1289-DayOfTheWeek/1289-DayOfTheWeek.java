24446188197e9c27f1692969b3e42e3a39d5fc61
Kirti Chaudhary <kirtichau1507@gmail.com>
2025-09-03 02:43:07 +0530

    Create 1289-DayOfTheWeek

---
+++ 1289-DayOfTheWeek/1289-DayOfTheWeek.java
@@ -0,0 +1,18 @@
+// Last updated: 9/3/2025, 2:43:04 AM
+class Solution {
+    public String dayOfTheWeek(int day, int month, int year) {
+        String[] week = {"Saturday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
+        if (month < 3) {
+            month += 12;
+            year -= 1;
+        }
+
+        int k = year % 100;
+        int j = year / 100;
+
+        int h = (day + (13 * (month + 1)) / 5 + k + (k / 4) + (j / 4) + 5 * j) % 7;
+        return week[h];
+   
+        
+    }
+}
