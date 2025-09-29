a1e2fb76f2bf22eb9a90a5dffc799843d120094f
Kirti Chaudhary <kirtichau1507@gmail.com>
2025-09-29 13:26:14 +0530

    Create 74-Searcha2DMatrix

---
+++ 74-Searcha2DMatrix/version/java/74-Searcha2DMatrix_20250929_132614.java
@@ -0,0 +1,15 @@
+// Last updated: 9/29/2025, 1:26:14 PM
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        for(int i=0;i< matrix.length;i++){
+            for(int j=0;j< matrix[0].length;j++){
+                if(matrix[i][j]==target){
+                    return true;
+                }
+
+            }
+        }
+        return false;
+        
+    }
+}
