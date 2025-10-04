d2b64f36463c579aa27a6e6477a145fb59a8e76e
Kirti Chaudhary <kirtichau1507@gmail.com>
2025-10-04 13:54:02 +0530

    Create 509-FibonacciNumber

---
+++ 509-FibonacciNumber/version/java/509-FibonacciNumber_20251004_135402.java
@@ -0,0 +1,11 @@
+// Last updated: 10/4/2025, 1:54:02 PM
+class Solution {
+    public int fib(int n) {
+        if(n==0)return 0;
+        if(n==1)return 1;
+        return fib(n-1)+fib(n-2);
+        
+        
+    }
+
+}
