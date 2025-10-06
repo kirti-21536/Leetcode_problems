2ad9e28d0793a7901c01d5443e2ae4570f7738c0
Kirti Chaudhary <kirtichau1507@gmail.com>
2025-10-06 20:22:06 +0530

    Create 746-MinCostClimbingStairs

---
+++ 746-MinCostClimbingStairs/version/java/746-MinCostClimbingStairs_20251006_202203.java
@@ -0,0 +1,26 @@
+// Last updated: 10/6/2025, 8:22:03 PM
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int n=cost.length;
+        //top down approach(memoization)
+        int dp[]=new int[n+1];
+        Arrays.fill(dp,-1);
+        return Math.min(climb(n-1,cost,dp),climb(n-2,cost,dp));
+    }
+    public int climb(int n,int cost[],int dp[]){
+        if(n<=1)return cost[n];
+        if(dp[n]!=-1)return dp[n];
+        return dp[n]=cost[n]+Math.min(climb(n-1,cost,dp),climb(n-2,cost,dp));
+    }
+        //bottom up approach(tabulation)
+    //     int dp[]=new int[n+1];
+    //     dp[0]=cost[0];
+    //     dp[1]=cost[1];
+    //     for(int i=2;i<n;i++){
+    //         dp[i]=cost[i]+Math.min(dp[i-1],dp[i-2]);
+    //     }
+    //     dp[n]=Math.min(dp[n-1],dp[n-2]);
+    //     return dp[n];
+
+    // }
+}
