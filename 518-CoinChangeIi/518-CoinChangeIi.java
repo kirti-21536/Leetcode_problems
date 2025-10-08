76e0ee6700b0b2c57658cd821456799fbb8424ec
Kirti Chaudhary <kirtichau1507@gmail.com>
2025-10-08 11:35:29 +0530

    Create 518-CoinChangeIi

---
+++ 518-CoinChangeIi/518-CoinChangeIi.java
@@ -0,0 +1,23 @@
+// Last updated: 10/8/2025, 11:35:29 AM
+class Solution {
+    public int change(int amount, int[] coins) {
+        int dp[][]=new int[amount+1][coins.length];
+        for(int[] a:dp){
+            Arrays.fill(a,-1);
+        }
+        return coin_change(coins,amount,0,dp);
+        
+    }
+    public int coin_change(int coins[],int amount,int i,int dp[][]){
+        if(amount==0)return 1;
+        if(i==coins.length)return 0;
+        if(dp[amount][i]!=-1)return dp[amount][i];
+
+        int inc=0,exc=0;
+        if(amount>=coins[i]){
+            inc=coin_change(coins,amount-coins[i],i,dp);
+        }
+        exc=coin_change(coins,amount,i+1,dp);
+        return dp[amount][i]=inc+exc;
+    }
+}
