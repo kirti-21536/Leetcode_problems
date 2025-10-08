4b7f4793359a077165dbacba6b5c7dba584f8c84
Kirti Chaudhary <kirtichau1507@gmail.com>
2025-10-09 00:32:51 +0530

    Create 20-ValidParentheses

---
+++ 20-ValidParentheses/version/java/20-ValidParentheses_20251009_003250.java
@@ -0,0 +1,32 @@
+// Last updated: 10/9/2025, 12:32:50 AM
+class Solution {
+    public boolean isValid(String s) {
+      
+        //      while(true){
+        //     if(s.contains("()"))s=s.replace("()","");
+        //     else if(s.contains("{}"))s=s.replace("{}","");
+        //     else if(s.contains("[]"))s=s.replace("[]","");
+        //     else return s.isEmpty();
+        // }
+        if(s.length()==1)return false;
+        Stack<Character> st=new Stack<>();
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            
+            if(ch=='(' || ch=='{' || ch=='[')st.push(ch);
+            else{
+                if(st.isEmpty())return false;
+                if(ch==')'&& st.peek()=='('
+                ||ch=='}' && st.peek()=='{'
+                ||ch==']'&& st.peek()=='['){
+                    st.pop();}  
+                    else{
+                        return false;
+                    } 
+            }
+        }
+        if(st.isEmpty())return true;
+        return false;
+   
+    }
+}
