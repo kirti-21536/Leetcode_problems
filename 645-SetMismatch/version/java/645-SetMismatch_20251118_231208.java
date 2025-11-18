1ea62946a3afcf63188908878e0d8c3efd21d0ab
Kirti Chaudhary <kirtichau1507@gmail.com>
2025-11-18 23:12:11 +0530

    Create 645-SetMismatch

---
+++ 645-SetMismatch/version/java/645-SetMismatch_20251118_231208.java
@@ -0,0 +1,23 @@
+// Last updated: 11/18/2025, 11:12:08 PM
+class Solution {
+    public List<String> buildArray(int[] arr, int n) {
+        List<String> ll=new ArrayList<>();
+        int k=0;
+        int i=1;
+        while(i<=n && k<arr.length){
+            if(arr[k]==i){
+                ll.add("Push");
+                k++;
+                i++;
+            }
+            else{
+                while(arr[k]>i){
+                    ll.add("Push");
+                    ll.add("Pop"); 
+                    i++;     
+            }
+            }
+        }
+        return ll;
+    }
+}
