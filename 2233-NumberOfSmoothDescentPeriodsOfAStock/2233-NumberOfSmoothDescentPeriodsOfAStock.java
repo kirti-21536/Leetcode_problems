dc1a615e0e48cf6ced1dacba5ad7e427b0bdc2b9
Kirti Chaudhary <kirtichau1507@gmail.com>
2025-12-20 23:55:47 +0530

    Create 2233-NumberOfSmoothDescentPeriodsOfAStock

---
+++ 2233-NumberOfSmoothDescentPeriodsOfAStock/2233-NumberOfSmoothDescentPeriodsOfAStock.java
@@ -0,0 +1,17 @@
+// Last updated: 12/20/2025, 11:55:45 PM
+class Solution {
+    public long getDescentPeriods(int[] prices) {
+        long ans = 1;    
+        long len = 1;     
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] == prices[i - 1] - 1) {
+                len++;
+            } else {
+                len = 1;
+            }
+            ans += len;
+        }
+
+        return ans;
+    }
+}
