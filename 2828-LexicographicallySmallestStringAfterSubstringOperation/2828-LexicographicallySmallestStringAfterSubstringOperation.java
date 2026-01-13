423bf466f6285459dadbfe066fe7e0f495c79bc5
Kirti Chaudhary <kirtichau1507@gmail.com>
2026-01-13 11:11:11 +0530

    Create 2828-LexicographicallySmallestStringAfterSubstringOperation

---
+++ 2828-LexicographicallySmallestStringAfterSubstringOperation/2828-LexicographicallySmallestStringAfterSubstringOperation.java
@@ -0,0 +1,20 @@
+// Last updated: 1/13/2026, 11:11:08 AM
+class Solution {
+    public String smallestString(String s) {
+        char arr[]=s.toCharArray();
+        int n=arr.length;
+        int i=0;
+        while(i<n && arr[i]=='a')i++;
+        if(i==n){
+            arr[n-1]='z';
+            return new String(arr);
+        }
+
+        while(i<n && arr[i]!='a'){
+            arr[i]=(char)(arr[i]-1);
+            i++;
+        }
+        return new String(arr);
+        
+    }
+}
