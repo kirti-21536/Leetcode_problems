b0fc392aaaa02b774f85659e6a8cedfa16aa8938
Kirti Chaudhary <kirtichau1507@gmail.com>
2026-03-14 12:06:26 +0530

    Create 200-NumberofIslands

---
+++ 200-NumberofIslands/version/java/200-NumberofIslands_20260314_120624.java
@@ -0,0 +1,29 @@
+// Last updated: 3/14/2026, 12:06:24 PM
+1class Solution {
+2    public int numIslands(char[][] grid) {
+3        int islands = 0;
+4        for(int i=0; i<grid.length; i++){
+5            for(int j=0; j<grid[0].length; j++){
+6                if(grid[i][j]=='1'){
+7                    islands++;
+8                    check(grid,i,j);
+9                }
+10            }
+11        }
+12
+13        return islands;
+14    }
+15
+16    public static void check(char[][] grid, int i, int j){
+17        if(i<0 || i>=grid.length || j<0 || j>=grid[0].length || grid[i][j]=='0'){
+18            return;
+19        }
+20
+21        grid[i][j] = '0';
+22
+23        check(grid,i+1,j);
+24        check(grid,i-1,j);
+25        check(grid,i,j+1);
+26        check(grid,i,j-1);
+27    }
+28}
