0e0acf2a7402b5306c9ff1eeaaa829637a460079
Kirti Chaudhary <kirtichau1507@gmail.com>
2026-06-28 18:24:19 +0530

    Create

---
+++ problem-1782651254213/version/java/problem-1782651254213_20260628_182414.java
@@ -0,0 +1,12 @@
+// Last updated: 6/28/2026, 6:24:14 PM
+1class Solution {
+2    public int maximumElementAfterDecrementingAndRearranging(int[] arr) {
+3        Arrays.sort(arr);
+4        arr[0] = 1;
+5        for (int i = 1; i < arr.length; i++) {
+6            arr[i] = Math.min(arr[i], arr[i - 1] + 1);
+7        }
+8        return arr[arr.length - 1];
+9        
+10    }
+11}
