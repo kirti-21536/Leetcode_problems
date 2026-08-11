8fe679808db17bc170bb3ba120949e4447430ff5
Kirti Chaudhary <kirtichau1507@gmail.com>
2026-08-11 10:06:34 +0530

    File updated at 8/11/2026, 10:06:33 AM

--- 217-ContainsDuplicate/217-ContainsDuplicate.java
+++ 217-ContainsDuplicate/217-ContainsDuplicate.java
@@ -1,25 +1,17 @@
-// Last updated: 8/1/2025, 8:15:27 PM
-class Solution {
-    public boolean containsDuplicate(int[] nums) {
-        Arrays.sort(nums);
-        int ch=1;
-        int ans=0;
-        for(int i=1;i<nums.length;i++){
-            if(nums[i]==nums[i-1]){
-                ch++;
-            }
-            else{
-                ans=Math.max(ans,ch);
-                ch=1;
-            }
-        }
-        ans=Math.max(ch,ans);
-        if(ans>=2){
-            return true;
-        }
-        else{
-            return false;
-        }
-        
-    }
-}
+// Last updated: 8/11/2026, 10:06:33 AM
+1class Solution {
+2    public boolean containsDuplicate(int[] nums) {
+3        Arrays.sort(nums);
+4        int n=nums.length;
+5        for(int i=1;i<n;i++){
+6            if(nums[i]==nums[i-1])return true;
+7        }
+8        // for(int i=0;i<n;i++){
+9        //     for(int j=i+1;j<n;j++){
+10        //         if(nums[i]==nums[j])return true;
+11        //     }
+12        // }
+13        return false;
+14        
+15    }
+16}
