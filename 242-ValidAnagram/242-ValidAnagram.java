24d583e231d7ac92b22b65a05c5804c29dc8fa02
Kirti Chaudhary <kirtichau1507@gmail.com>
2026-08-12 13:48:56 +0530

    File updated at 8/12/2026, 1:48:55 PM

--- 242-ValidAnagram/242-ValidAnagram.java
+++ 242-ValidAnagram/242-ValidAnagram.java
@@ -1,11 +1,11 @@
-// Last updated: 8/1/2025, 8:15:15 PM
-class Solution {
-    public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) return false;
-        char[] a = s.toCharArray();
-        char[] b = t.toCharArray();
-        Arrays.sort(a);
-        Arrays.sort(b);
-        return Arrays.equals(a, b);
-    }
-}
+// Last updated: 8/12/2026, 1:48:55 PM
+1class Solution {
+2    public boolean isAnagram(String s, String t) {
+3        char[] arr1=s.toCharArray();
+4        char[] arr2=t.toCharArray();
+5        Arrays.sort(arr1);
+6        Arrays.sort(arr2);
+7        return Arrays.equals(arr1,arr2);
+8        
+9    }
+10}
